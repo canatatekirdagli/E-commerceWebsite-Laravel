586bd33ed8b2cca9baf712175c837fb22346f4aa
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2023-03-23 22:50:48 +0300

    Ürün içeriğinin ayarlanması ve düzenlenmesi

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -13,8 +13,9 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="panel panel-default">
-                    <div class="panel-heading"></div>
+                    <div class="panel-heading">{{$kategori->kategori_adi}}</div>
                     <div class="panel-body">
+                        @if(count($alt_kategoriler)>0)
                         <h3>Alt Kategoriler</h3>
                         <div class="list-group categories">
                             @foreach($alt_kategoriler as $alt_kategori)
@@ -24,57 +25,32 @@
                                 </a>
                             @endforeach
                         </div>
-                        <h3>Fiyat Aralığı</h3>
-                        <form>
-                            <div class="form-group">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox"> 100-200
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox"> 200-300
-                                    </label>
-                                </div>
-                            </div>
-                        </form>
+                        @else
+                        Bu kategoride başka alt kategori bulunmamaktadır.
+                        @endif
                     </div>
                 </div>
             </div>
             <div class="col-md-9">
                 <div class="products bg-content">
+                    @if(count($urunler)> 0)
                     Sırala
                     <a href="#" class="btn btn-default">Çok Satanlar</a>
                     <a href="#" class="btn btn-default">Yeni Ürünler</a>
                     <hr>
+                    @endif
                     <div class="row">
+                        @if(count($urunler)==0)
+                           <div class="col-md-12">Bu kategoride ürün bulunamadı!</div>
+                        @endif
+                        @foreach($urunler as $urun)
                         <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
+                            <a href="{{route('urun',$urun->slug)}}"><img src="http://lorempixel.com/400/400/food/1"></a>
+                            <p><a href="{{route('urun',$urun->slug)}}"> {{$urun->urun_adi}}</a></p>
+                            <p class="price">{{$urun->fiyati}} ₺</p>
                             <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
                         </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                            <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                            <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                            <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,10 +2,9 @@
     @yield('footer')
     <div class="container">
         <div class="row">
-            <div class="col-md-6">Eticaret Yazılımı &copy; 2018</div>
+            <div class="col-md-6">Can Ata Tekirdağlı &copy; 2023</div>
             <div class="col-md-6 text-right">
-                <a href="https://www.udemy.com/laravel-ile-sifirdan-eticaret-projesi">Udemy Kurs Sayfası</a> |
-                <a href="http://www.uzaktankurs.com">Uzaktan Kurs</a>
+                <div>E-Ticaret Sitesi</div>
             </div>
         </div>
     </div>
