22a490fe1020f40e693acb610bbec1dd4d4884b5
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2023-04-11 22:43:50 +0300

    Kullanıcı yapısının oluşturulmaya başlanması(Migration ve model dosyalarının eklenmesi) Kullanıcı kayıt işlemi yapılması ve kayıt edilirken gerekli kontrollerin yapılması. Lang dosyalarının türkçesiyle değiştirilmesi

--- app/Http/Controllers/KullaniciController.php
+++ app/Http/Controllers/KullaniciController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kullanici;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class KullaniciController extends Controller
 {
@@ -14,5 +16,24 @@ class KullaniciController extends Controller
     public function kaydol_form()
     {
         return view('kullanici.kaydol');
+    }
+
+
+    public function kaydol()
+    {
+        $this->validate(request(),[
+           'adsoyad'=>'required|min:5|max:60',
+            'email'=>'required|email|unique:kullanici',
+            'sifre'=>'required|confirmed|min:5|max:15'
+        ]);
+
+        $kullanici=Kullanici::create([
+            'adsoyad'=>request('adsoyad'),
+            'email'=>request('email'),
+            'sifre'=>\Hash::make(request('sifre')),
+            'aktivasyon_anahtari'=>Str::random(60),
+        ]);
+        auth()->login($kullanici);
+        return redirect()->route('anasayfa');
     }
 }
---
+++ app/Models/Kullanici.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Kullanici extends Authenticatable
+{
+    use SoftDeletes;
+
+    protected $table="kullanici";
+
+    const CREATED_AT = 'olusturma_tarihi';
+    const UPDATED_AT = 'guncelleme_tarihi';
+
+    protected $fillable = ['adsoyad', 'email', 'sifre','aktivasyon_anahtari','aktif_mi'];
+    protected $hidden = ['sifre', 'aktivasyon_anahtari'];
+}
